9956844678b11fdf103bd8895109d213c8e0f078
enoTak <eno.sleepy.zzz.zz.z@gmail.com>
2022-01-23 17:01:39 +0900

    [impl] login/out layout

--- application/flask_blog/views/entries.py
+++ application/flask_blog/views/entries.py
@@ -8,6 +8,8 @@ from flask import (
     session
 )
 
+from flask_login import login_required
+
 from flask_blog import app
 from flask_blog.models.entries import Entry
 
@@ -15,6 +17,7 @@ from datetime import datetime
 
 
 @app.route("/")
+@login_required
 def show_entries():
     entries = Entry.scan()
     entries = sorted(entries, key=lambda x: x.id, reverse=True)
@@ -22,6 +25,7 @@ def show_entries():
 
 
 @app.route("/entries", methods=["POST"])
+@login_required
 def add_entry():
     entry = Entry(
         id=int(datetime.now().timestamp()),
@@ -33,23 +37,27 @@ def add_entry():
 
 
 @app.route("/entries/new", methods=["GET"])
+@login_required
 def new_entry():
     return render_template("entries/new.html")
 
 
 @app.route("/entries/<int:id>", methods=["GET"])
+@login_required
 def show_entry(id):
     entry = Entry.get(id)
     return render_template("entries/show.html", entry=entry)
 
 
 @app.route("/entries/<int:id>/edit", methods=["GET"])
+@login_required
 def edit_entry(id):
     entry = Entry.get(id)
     return render_template("entries/edit.html", entry=entry)
 
 
 @app.route("/entries/<int:id>/update", methods=["POST"])
+@login_required
 def update_entry(id):
     entry = Entry.get(id)
     entry.title = request.form["title"]
@@ -59,6 +67,7 @@ def update_entry(id):
 
 
 @app.route("/entries/<int:id>/delete", methods=["POST"])
+@login_required
 def delete_entry(id):
     entry = Entry.get(id)
     entry.delete()
